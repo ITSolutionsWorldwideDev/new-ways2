d736110e7db55f9f72d5a32c5e141a2fe6ca1486
Amer Mushtaq <amer.mushtaq@itsolutionshub2010.com>
2025-09-16 10:45:34 +0500

    site default themes update

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -10,7 +10,8 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="dark"
+      // defaultTheme="dark"
+      defaultTheme="light" // set global default to light
       enableSystem={false}
       {...props}
     >
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -3,12 +3,32 @@
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 export function ThemeToggle() {
+
+
+  const pathname = usePathname();
   const { theme, setTheme } = useTheme();
 
+  // Auto-set theme based on route
+  useEffect(() => {
+    if (pathname === "/") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, [pathname, setTheme]);
+
+
   return (
-    <Button
+    <></>
+  );
+}
+
+/* 
+<Button
       variant="ghost"
       size="icon"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
@@ -18,5 +38,4 @@ export function ThemeToggle() {
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
-  );
-}
+*/
